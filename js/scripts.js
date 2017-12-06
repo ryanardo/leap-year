b3fa810e1d6a2344b842a9e4d712c8b14b3be1c9
Ryan G Ferris <ryanglenferris@gmail.com>
2017-12-05 21:49:31 -0800

    create first spec to test if year input is leap-year

---
+++ js/scripts.js
@@ -0,0 +1,18 @@
+//Back end logic
+
+var leapYear = function(year) {
+  return false;
+}; //End of 'leapYear' function
+
+
+
+
+//Front end logic
+$(document).ready(function() {
+  $("form#leap-year").submit(function(event) {
+    event.preventDefault();
+    var year = parseInt($("input#year").val());
+    var result = leapYear(year);
+    $("#result").text(result);
+  }); //End of 'submit' listener
+}); //End of 'document ready' function
